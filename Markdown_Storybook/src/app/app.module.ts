ba3067789a17e31656062dec0bbeb98c0bccb6de
sujith surendran <sujithsurendran4433@gmail.com>
2023-05-10 12:52:44 +0530

    folder mermaid added

--- Markdown_Storybook/src/app/app.module.ts
+++ Markdown_Storybook/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MermaidComponent } from './mermaid/mermaid.component';
+
+
 import { MarkdownModule } from 'ngx-markdown';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MermaidComponent
   ],
   imports: [
     BrowserModule,
